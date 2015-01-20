483fa70657c236d78edd8a94f471aac8f7acaeea
Russell <rparde@ocuvera.com>
2015-01-20 16:55:38 -0600

    Added search, and Rating

--- MVCMovie3/Controllers/MoviesController.cs
+++ MVCMovie3/Controllers/MoviesController.cs
@@ -15,9 +15,31 @@ namespace MVCMovie3.Controllers
         private MovieDBContext db = new MovieDBContext();
 
         // GET: Movies
-        public ActionResult Index()
+        public ActionResult Index(string movieGenre, string searchString)
         {
-            return View(db.Movies.ToList());
+            var GenreLst = new List<string>();
+
+            var GenreQry = from d in db.Movies
+                           orderby d.Genre
+                           select d.Genre;
+
+            GenreLst.AddRange(GenreQry.Distinct());
+            ViewBag.movieGenre = new SelectList(GenreLst);
+           
+            var movies = from m in db.Movies
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            return View(movies);
         }
 
         // GET: Movies/Details/5
@@ -46,7 +68,7 @@ namespace MVCMovie3.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +100,7 @@ namespace MVCMovie3.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
---
+++ MVCMovie3/fonts/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MVCMovie3.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyy-MM-dd}", ApplyFormatInEditMode = true)]
+        
+        public DateTime ReleaseDate { get; set; }
+        
+        public string Genre { get; set; }
+        
+        public decimal Price { get; set; }
+
+        public string Rating { get; set; }
+
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
